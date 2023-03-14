df26a772538990e60a2e96964a7bc2a28b48ecf9
Grant Bartlett <hello@grant-bartlett.com>
2023-03-14 13:27:06 +0000

    Remove rename sync logs

--- src/rename_logs.asm
+++ src/rename_logs.asm
@@ -2,9 +2,9 @@
 %include "macros/datatypes.inc"
 
 sstring str_except, "EXCEPT_CNCNET.TXT"
-sstring str_sync1, "SYNC_CNCNET%01d.TXT"
-sstring str_sync2, "SYNC_CNCNET%01d_%03d.TXT"
+; sstring str_sync1, "SYNC_CNCNET%01d.TXT"
+; sstring str_sync2, "SYNC_CNCNET%01d_%03d.TXT"
 
 @SET 0x004C901C, {push str_except}
-@SET 0x0064DEC4, {push str_sync1}
-@SET 0x00651716, {push str_sync2}
+; @SET 0x0064DEC4, {push str_sync1}
+; @SET 0x00651716, {push str_sync2}
