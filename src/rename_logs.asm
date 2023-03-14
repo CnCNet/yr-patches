80ca4b94ac6f0b0ca0d8d57dfd1b27ad72c1e488
Grant Bartlett <hello@grant-bartlett.com>
2023-03-14 12:00:54 +0000

    Revert cloaked unit patch and sync log name change

--- src/rename_logs.asm
+++ src/rename_logs.asm
@@ -2,8 +2,8 @@
 %include "macros/datatypes.inc"
 
 sstring str_except, "EXCEPT_CNCNET.TXT"
-sstring str_sync1, "SYNC_CNCNET%01d.TXT"
-sstring str_sync2, "SYNC_CNCNET%01d_%03d.TXT"
+sstring str_sync1, "SYNC%01d.TXT"
+sstring str_sync2, "SYNC%01d_%03d.TXT"
 
 @SET 0x004C901C, {push str_except}
 @SET 0x0064DEC4, {push str_sync1}
